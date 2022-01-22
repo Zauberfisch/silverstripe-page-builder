9f12c368b66a17266a7803d348d1a49629032fad
Zauberfisch <zauberfisch@zauberfisch.at>
2022-01-22 13:41:30 +0100

    update

--- src/Form/Field.php
+++ src/Form/Field.php
@@ -4,29 +4,5 @@ declare(strict_types=1);
 
 namespace zauberfisch\PageBuilder\Form;
 
-use SilverStripe\Forms\FormField;
-use SilverStripe\ORM\DataObjectInterface;
-
-/**
- * @author zauberfisch
- */
-class Field extends FormField {
-	protected PageBuilderConfig $config;
-
-	public function __construct($name, $title, PageBuilderConfig $config) {
-		$this->config = $config;
-		$this->addExtraClass('zauberfisch__page-builder__field');
-		parent::__construct($name, $title, $this->config->getArea()->ElementsData);
-	}
-
-	public function getSchemaData() {
-		$arr = parent::getSchemaData();
-		$arr['elements'] = $this->config->getElementMap();
-		$arr['value'] = $this->dataValue();
-		return $arr;
-	}
-
-	public function saveInto(DataObjectInterface $record) {
-		$this->config->saveInto($this->dataValue());
-	}
+class Field extends PageBuilderField {
 }
---
+++ src/Form/PageBuilderField.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace zauberfisch\PageBuilder\Form;
+
+use SilverStripe\Forms\FormField;
+use SilverStripe\ORM\DataObjectInterface;
+
+class PageBuilderField extends FormField {
+	protected PageBuilderConfig $config;
+
+	public function __construct($name, $title, PageBuilderConfig $config) {
+		$this->config = $config;
+		$this->addExtraClass('zauberfisch__page-builder__field');
+		parent::__construct($name, $title, $this->config->getArea()->ElementsData);
+	}
+
+	public function getSchemaData() {
+		$arr = parent::getSchemaData();
+		$arr['elements'] = $this->config->getElementMap();
+		$arr['value'] = $this->dataValue();
+		return $arr;
+	}
+
+	public function saveInto(DataObjectInterface $record) {
+		$this->config->saveInto($this->dataValue());
+	}
+}
