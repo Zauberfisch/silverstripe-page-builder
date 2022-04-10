e2bd074ff463d72ea85e2a128ddaceb1b95cee86
Zauberfisch <zauberfisch@zauberfisch.at>
2022-04-10 10:14:44 +0200

    Modal close title

--- client/src/components/PageBuilder/modals/EditFormModal.js
+++ client/src/components/PageBuilder/modals/EditFormModal.js
@@ -1,32 +1,33 @@
 import React from "react"
-import {Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap"
+import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from "reactstrap"
 import {FormButtonComponent} from "../form"
 
-export function EditFormModal({isOpen, onClickClose, children, footerChildren}) {
+export function EditFormModal({isOpen, title, onClickClose, children, footerChildren, closeTitle}) {
 	return (
 		<Modal isOpen={isOpen} toggle={onClickClose}>
 			<ModalHeader toggle={onClickClose}>
-				Buttons
+				{title}
 			</ModalHeader>
 			<ModalBody>
 				<div style={{overflow: "auto", maxHeight: "70vh"}}>
 					{children}
 				</div>
 			</ModalBody>
-			{React.Children.count(footerChildren) > 0 ? <ModalFooter>
+			{React.Children.count(footerChildren) > 0 || closeTitle ? <ModalFooter>
 				{footerChildren}
+				{closeTitle ? <Button color="success" onClick={onClickClose}>{closeTitle}</Button> : null}
 			</ModalFooter> : null}
 		</Modal>
 	)
 }
 
-export function EditFormModalWithButton({buttonTitle = "Edit", buttonProps, children, footerChildren}) {
+export function EditFormModalWithButton({buttonTitle = "Edit", title, buttonProps, children, footerChildren, closeTitle}) {
 	const [isOpen, setIsOpen] = React.useState(false)
 	const toggleModal = React.useCallback(() => setIsOpen(oldVal => !oldVal), [])
 	return (
 		<React.Fragment>
 			<FormButtonComponent {...buttonProps} onClick={toggleModal}>{buttonTitle}</FormButtonComponent>
-			<EditFormModal isOpen={isOpen} children={children} footerChildren={footerChildren} onClickClose={toggleModal} />
+			<EditFormModal isOpen={isOpen} title={title} children={children} footerChildren={footerChildren} onClickClose={toggleModal} closeTitle={closeTitle} />
 		</React.Fragment>
 	)
 }
